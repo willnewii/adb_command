6d1753071e58021b9daf59f1dd7574d4548e4aaa
诗人的咸鱼 <willnewii@gmail.com>
2016-12-19 16:30:52 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+根据🍭 ADB Usage Complete / ADB 用法大全](https://github.com/mzlogin/awesome-adb)这篇文章,把一些常用的命令做了封装.
+
+### 功能
+- 查看设备信息
+- 查看包列表
+- 查看apk信息(支持模糊查询)
+- 查看ip信息
+- 截图(截屏,上传文件,删除)
+- 截屏(同上)
+
+### 资料
+- [🍭 ADB Usage Complete / ADB 用法大全](https://github.com/mzlogin/awesome-adb)
+- [sed & awk & grep 专题( 鸟哥 )](http://www.cnblogs.com/moveofgod/p/3540575.html)
+- [Python 命令行输出的颜色设置 ](http://blog.chinaunix.net/uid-27714502-id-4110758.html)
+- [Python_cmd的各种实现方法及优劣（subprocess.Popen, os.system和commands.getstatusoutput）](http://blog.csdn.net/menglei8625/article/details/7494094)
---
+++ adb_command.py
@@ -0,0 +1,117 @@
+# -*- conding:uft-8 -*-
+import os
+import sys
+import subprocess
+import time
+
+ads = 'adb shell '
+
+def _command(command):
+    #os.system('adb devices')
+    #os.popen('adb devices').read()
+    status, output = subprocess.getstatusoutput(command)
+    return output
+
+#输出设备信息
+def deviceInfo():
+    brand = _command(ads + 'getprop ro.product.brand')
+    name = _command(ads + 'getprop ro.product.name')
+    model = _command(ads + 'getprop ro.product.model')
+#    print('%s %s (%s)' % (brand,model,name))
+    print ('品牌:' + brand)
+    print ('型号:' + model)
+    print ('代号:' + name)
+    print ('系统版本:' + _command(ads + 'getprop ro.build.version.release') + ',' + _command(ads + 'getprop ro.build.version.sdk'))
+    print ('屏幕密度:' + _command(ads + 'wm density'))
+    print ('屏幕分辨率:' + _command(ads + 'wm size'))
+
+#输出IP地址
+def deviceIP():
+    os.system(ads + 'ifconfig | grep Mask')
+
+#屏幕截图
+def screencap():
+    fileName = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime());
+    os.system(ads + 'screencap -p /sdcard/' + fileName + '.png')
+    os.system('adb pull /sdcard/' + fileName + '.png')
+    os.system(ads + 'rm /sdcard/' + fileName + '.png')
+
+#录制屏幕
+def screenrecord():
+    print('屏幕录制中...需要停止时按ctrl+c')
+    fileName = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime());
+    os.system(ads + 'screenrecord /sdcard/' + fileName + '.mp4')
+    time.sleep(1)
+    os.system('adb pull /sdcard/' + fileName + '.mp4')
+    os.system(ads + 'rm /sdcard/' + fileName + '.mp4')
+
+#显示包列表
+#word:搜索关键字
+def packages(word):
+    if len(word)==0:
+        os.system(ads + 'pm list packages')
+    else:
+        os.system(ads + 'pm list packages | grep ' + word)
+
+def apkInfo(package):
+    if len(package)==0:
+        print('抱歉,请输入你要搜索的包名')
+    else:
+        com = ads + 'pm list packages ' + package + "| awk -F ':' '{print $2}'"
+        packagesStr = _command(com)
+
+        if len(packagesStr)==0:
+            print('没有匹配到包:' + package)
+        else:
+            packages = packagesStr.split('\n')
+            for index in range(len(packages)):
+                print(index,packages[index])
+
+            num = int(input("请选择你要查询应用:"))
+            os.system("adb shell dumpsys package " + packages[num])
+
+#输出帮助信息
+def printHelp():
+    print('方法列表:')
+    print('info             ·········  获取设备信息')
+    print('apkinfo          ·········  获取指定包名的apk信息 支持模糊查询')
+    print('ip               ·········  获取ip信息')
+    print('packages         ·········  获取安装包列表 可跟关键字. eg:packages weibo')
+    print('cap              ·········  截取当前屏幕,保存在当前目录,然后删除手机中的截图文件')
+    print('screenrecord     ·········  录制当前屏幕,保存在当前目录,然后删除手机中的录屏文件')
+    print('v 0.0.1 2016-12-19 16:04:44')
+
+def test():
+    print (time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()))
+'''    os.system(ads + 'input keyevent 64')
+    os.system(ads + 'input keyevent 99')
+    os.system(ads + 'input text aaaa')
+'''
+if len(sys.argv) ==1:
+    deviceInfo()
+elif sys.argv[1]=='info':
+    deviceInfo()
+elif sys.argv[1]=='cap':
+    screencap()
+elif sys.argv[1]=='ip':
+    deviceIP()
+elif sys.argv[1]=='packages':
+    if len(sys.argv) ==2:
+        packages('')
+    else:
+        packages(sys.argv[2])
+elif sys.argv[1]=='help':
+    printHelp()
+elif sys.argv[1]=='screenrecord':
+    screenrecord()
+elif sys.argv[1]=='apkinfo':
+    if len(sys.argv) ==2:
+        apkInfo('')
+    else:
+        apkInfo(sys.argv[2])
+
+elif sys.argv[1]=='test':
+    test()
+else:
+    print('未匹配到命令')
+    printHelp()
