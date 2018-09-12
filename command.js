82452387715289967c79a0f787a5d2433936bcaf
诗人的咸鱼 <willnewii@163.com>
2018-09-12 14:23:07 +0800

    update

--- command.js
+++ command.js
@@ -1,12 +1,12 @@
 const prompt = require('prompt');
 //const colors = require("colors");
-const colors = require("chalk");
+const chalk = require("chalk");
 const program = require('commander');
 
 const execSync = require('child_process').execSync
 
-const highlightBG = colors.bgBlue;
-const highlight = colors.bold.blue;
+const highlightBG = chalk.bgBlue;
+const highlight = chalk.bold.blue;
 
 //import mortgage from './js/mortgage';
 const mortgage = require('./js/mortgage');
@@ -14,91 +14,96 @@ const mortgage = require('./js/mortgage');
 prompt.message = '';
 prompt.delimiter = highlight(prompt.delimiter);
 
-const run_command = function (command) {
-  let result;
-  try {
-    result = execSync(command);
-  } catch (ex) {
-    result = '';
-  }
-  return result.toString().trim();
+const run_command = function(command) {
+    let result;
+    try {
+        result = execSync(command);
+    } catch (ex) {
+        result = '';
+    }
+    return result.toString().trim();
 }
 
 //获取输入内容
 async function getInput(tip) {
-  let schema = {
-    properties: {
-      name: {
-        description: highlightBG(tip),
-        required: true
-      }
+    let schema = {
+        properties: {
+            name: {
+                description: highlightBG(tip),
+                required: true
+            }
+        }
     }
-  }
 
-  let result = await new Promise(function (resolve, reject) {
-    prompt.start();
-    prompt.get(schema, (error, result) => {
-      if (!error) {
-        resolve(result.name);
-      } else {
-        reject(error);
-      }
+    let result = await new Promise(function(resolve, reject) {
+        prompt.start();
+        prompt.get(schema, (error, result) => {
+            if (!error) {
+                resolve(result.name);
+            } else {
+                reject(error);
+            }
+        });
     });
-  });
 
-  return result;
+    return result;
 }
 
 /**
  * 根据匹配端口号 kill相应进程
  */
 async function doKillByPort(_port) {
-  //lsof -i -n -P | grep :80
-  let command = 'lsof -i tcp';
-  if (_port) {
-    command = command + ':' + _port
-  }
-
-  let result = run_command(command);
-  if (!result) {
-    console.log(highlightBG('没有查到匹配的进程 执行命令:' + command));
-    return;
-  }
-
-  let results = result.split('\n');
-
-  results.forEach((item, index) => {
-    if (index > 0) {
-      console.log(highlight(index) + '  :  ' + item);
+    //lsof -i -n -P | grep :80
+    let command = 'lsof -i tcp';
+    if (_port) {
+        command = command + ':' + _port
     }
-  })
-  let index = await getInput('请输入你要关闭的进程编号');
-  let reg = /\S+/g; //匹配非空白符
-  let port = results[parseInt(index)].match(reg)[1]
 
-  run_command('kill -9 ' + port)
-  console.log('finish');
+    let result = run_command(command);
+    if (!result) {
+        console.log(highlightBG('没有查到匹配的进程 执行命令:' + command));
+        return;
+    }
+
+    let results = result.split('\n');
+
+    results.forEach((item, index) => {
+        if (index > 0) {
+            console.log(highlight(index) + '  :  ' + item);
+        }
+    })
+    let index = await getInput('请输入你要关闭的进程编号');
+    let reg = /\S+/g; //匹配非空白符
+    let port = results[parseInt(index)].match(reg)[1]
+
+    run_command('kill -9 ' + port)
+    console.log('finish');
 }
 
 function doMortgage() {
-  let m = new mortgage();
+    let m = new mortgage();
 
-  let result = m.print();
-  console.log(result);
-  //run_command('say ' + result.replace(/\n/g, '') + '    啦啦啦啦啦啦啦');
+    let result = m.print();
+    // console.log(result);
+    console.log(`第${highlight(result.month)}期应还款: ${highlight(result.yh)} 元
+
+待还款: ${highlight(result.dh)} 元
+
+坚持一下你还有 ${highlight(result.year)} 年就能还完啦~`);
+    //run_command('say ' + result.replace(/\n/g, '') + '    啦啦啦啦啦啦啦');
 }
 
 function doTest() {
-  console.log('test');
+    console.log('test');
 }
 
 program
-  .version(require('./package.json').version)
-  .option('-t, --test', '测试', doTest)
-  .option('-p, --port <port>', 'kill相应进程', doKillByPort)
-  .option('-f, --fangdai', '房贷计算器', doMortgage)
-  .parse(process.argv);
+    .version(require('./package.json').version)
+    .option('-t, --test', '测试', doTest)
+    .option('-p, --port <port>', 'kill相应进程', doKillByPort)
+    .option('-f, --fangdai', '房贷计算器', doMortgage)
+    .parse(process.argv);
 
 if (program.rawArgs.length <= 2) {
-  program.help();
+    program.help();
 }
--- js/mortgage.js
+++ js/mortgage.js
@@ -9,77 +9,83 @@ const startMonth = 8;
  * 房贷计算器
  */
 class mortgage {
-  /**
-   * @param total         贷款总额
-   * @param totalmonth    还款月数
-   * @param rate          贷款利率
-   * @param _startYear    还款开始年
-   * @param _startMonth   还款开始月
-   */
-  constructor(total, totalmonth, rate, _startYear, _startMonth) {
-    this.total = total || TOTAL;
-    this.totalmonth = totalmonth || TOTALMONTH;
-    this.rate = rate || RATE;
-    this.startYear = _startYear || startYear;
-    this.startMonth = _startMonth || startMonth;
+    /**
+     * @param total         贷款总额
+     * @param totalmonth    还款月数
+     * @param rate          贷款利率
+     * @param _startYear    还款开始年
+     * @param _startMonth   还款开始月
+     */
+    constructor(total, totalmonth, rate, _startYear, _startMonth) {
+        this.total = total || TOTAL;
+        this.totalmonth = totalmonth || TOTALMONTH;
+        this.rate = rate || RATE;
+        this.startYear = _startYear || startYear;
+        this.startMonth = _startMonth || startMonth;
 
-    //月还款本金
-    this.principal = this.total / this.totalmonth;
-  }
+        //月还款本金
+        this.principal = this.total / this.totalmonth;
+    }
 
-  /**
-   * 当月应还
-   */
-  print() {
-    let currentMonth = this.currentNumMonth();
-    let monthInterest = this.calculateMonthInterest(currentMonth);
-    let totalSum = this.calculateTotalSum();
+    /**
+     * 当月应还
+     */
+    print() {
+        let currentMonth = this.currentNumMonth();
+        let monthInterest = this.calculateMonthInterest(currentMonth);
+        let totalSum = this.calculateTotalSum();
 
-    return `第${currentMonth}期应还款:${mortgage.handleNumPrint(monthInterest + this.principal)}元 ` + '\n' +
-    //`已还款:${mortgage.handleNumPrint(this.calculateRepayment(currentMonth - 1))}元 ` + '\n' +
-    `待还款:${mortgage.handleNumPrint((totalSum - this.calculateRepayment(currentMonth - 1)))}元 ` + '\n' +
-    `坚持一下你还有${mortgage.handleNumPrint((this.totalmonth-currentMonth)/12)}年就能还完啦~`
-  }
+        return {
+          month :currentMonth ,
+          yh:handleNumPrint(monthInterest + this.principal),
+          dh:handleNumPrint((totalSum - this.calculateRepayment(currentMonth - 1))),
+          year:handleNumPrint((this.totalmonth-currentMonth)/12)
+        }
 
-  /**
-   * 已还金额
-   */
-  calculateRepayment(numMonth) {
-    /*        let total = 0;
-            for (let i = 1; i <= numMonth; i++) {
-                total = total + (this.calculateMonthInterest(i) + this.principal);
-            }*/
-    //平均利息
-    let temp1 = (this.calculateMonthInterest(1) + this.calculateMonthInterest(numMonth)) / 2;
-    return (temp1 + this.principal) * numMonth;
-  }
+        /* return `第${currentMonth}期应还款:${handleNumPrint(monthInterest + this.principal)}元 ` + '\n\n' +
+            //`已还款:${mortgage.handleNumPrint(this.calculateRepayment(currentMonth - 1))}元 ` + '\n' +
+            `待还款:${handleNumPrint((totalSum - this.calculateRepayment(currentMonth - 1)))}元 ` + '\n\n' +
+            `坚持一下你还有${handleNumPrint((this.totalmonth-currentMonth)/12)}年就能还完啦~` */
+    }
 
-  calculateMonthInterest(numMonth) {
-    return (this.total - (numMonth - 1) * this.principal) * this.rate / 12;
-  }
+    /**
+     * 已还金额
+     */
+    calculateRepayment(numMonth) {
+        //平均利息
+        let temp1 = (this.calculateMonthInterest(1) + this.calculateMonthInterest(numMonth)) / 2;
+        return (temp1 + this.principal) * numMonth;
+    }
 
-  /**
-   * 总利息=〔(总贷款额÷还款月数+总贷款额×月利率)+总贷款额÷还款月数×(1+月利率)〕÷2×还款月数-总贷款额
-   */
-  calculateTotalSum() {
-    //return ((this.principal + this.total * this.rate / 12) + this.total / this.totalmonth * (1 + this.rate / 12)) / 2 * this.totalmonth - this.total;
-    return this.calculateRepayment(this.totalmonth);
-  }
+    /**
+     * 当月利息
+     */
+    calculateMonthInterest(numMonth) {
+        return (this.total - (numMonth - 1) * this.principal) * this.rate / 12;
+    }
 
-  /**
-   * 获取当前还款期数
-   */
-  currentNumMonth() {
-    let date = new Date();
-    let year = date.getFullYear();
-    let month = date.getMonth() + 1;
+    /**
+     * 总利息=〔(总贷款额÷还款月数+总贷款额×月利率)+总贷款额÷还款月数×(1+月利率)〕÷2×还款月数-总贷款额
+     */
+    calculateTotalSum() {
+        return this.calculateRepayment(this.totalmonth);
+    }
 
-    return (year - this.startYear) * 12 + (month - this.startMonth) + 1;
-  }
+    /**
+     * 获取当前还款期数
+     */
+    currentNumMonth() {
+        let date = new Date();
+        let year = date.getFullYear();
+        let month = date.getMonth() + 1;
 
-  static handleNumPrint(result) {
-    return parseFloat(result).toFixed(2);
-  }
+        return (year - this.startYear) * 12 + (month - this.startMonth) + 1;
+    }
+
+}
+
+function handleNumPrint(result) {
+    return parseFloat(result).toFixed(2).replace(/\d{1,3}(?=(\d{3})+(\.\d*)?$)/g, '$&,');
 }
 
 module.exports = mortgage;
